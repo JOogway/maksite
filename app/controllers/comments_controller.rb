ee799e262b94abf4b0267fd9409a3aaece1f9727
JOogway <mak.zieli@gmail.com>
2018-07-24 18:15:06 +0200

    refactor

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,20 +1,23 @@
 class CommentsController < ApplicationController
 	before_action :find_article, :authorize
 	def create
-		@comment = @article.comments.create(params[:comment].permit(:content))	
-		if current_user
-		@comment.user_id = current_user.id
-		@comment.save
-		elsif current_admin_user
+		@article = Article.find(params[:article_id])
+        @comment = @article.comments.create(params[:comment].permit(:name, :comment))
+        redirect_to article_path(@article)
+		#@comment = @article.comments.create(params[:comment].permit(:content))	
+		#if current_user
 		#@comment.user_id = current_user.id
-		@comment.save
-		end
+		#@comment.save
+		#elsif current_admin_user
+		#@comment.user_id = current_user.id
+		#@comment.save
+		#end
 
-		if @comment.save
-			redirect_to article_path(@article)
-		else
-			render 'new'
-		end
+		#if @comment.save
+		#	redirect_to article_path(@article)
+		#else
+		#	render 'new'
+		#end
 	end
 
 	private
