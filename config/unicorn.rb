0057e74ec6015ab68b9607b847b76e55af933e65
JOogway <mak.zieli@gmail.com>
2018-08-14 16:20:27 +0200

    FIX unicorn config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,7 +1,7 @@
 # set path to the application
 app_dir File.expand_path("../..", __FILE__)
-shared_dir = "#{app_dir}/shared"
-working_directory app_dir
+shared_dir = "#{maksite}/shared"
+working_directory maksite
 
 # Set unicorn options
 worker_processes 2
